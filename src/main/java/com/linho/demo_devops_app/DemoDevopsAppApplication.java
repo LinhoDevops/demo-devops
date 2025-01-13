fc4116d6b130c4cdc1b49af10f85241300abf3fb
linhoDevops <jadonplimesancho117@gmail.com>
2025-01-13 13:22:38 +0000

    devops commit

---
+++ src/main/java/com/linho/demo_devops_app/DemoDevopsAppApplication.java
@@ -0,0 +1,13 @@
+package com.linho.demo_devops_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDevopsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDevopsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linho/demo_devops_app/DemoDevopsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linho.demo_devops_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDevopsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
